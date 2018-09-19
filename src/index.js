6f8e91a036632387c5afb4fc1654f7e1cbcd9799
ogurez <ogurezslava@gmail.com>
2018-09-19 17:35:36 +0300

    first

--- src/index.js
+++ src/index.js
@@ -1,30 +1,56 @@
 class Sorter {
+  
   constructor() {
     // your implementation
+    this.arr=[];
+    let comparator=null;
+    this.length=this.arr.length;
+    
   }
 
   add(element) {
     // your implementation
+    this.arr.push(element);
+    this.length++;
   }
 
   at(index) {
     // your implementation
+    return this.arr[index];
   }
 
   get length() {
     // your implementation
+    return this.length;
   }
 
   toArray() {
     // your implementation
+    return this.arr;
   }
 
   sort(indices) {
     // your implementation
+    let newArr=[];
+    for(let i=0; i<indices.length; i++){
+      newArr.push(this.arr[indices[i]]);
+    }
+      if(comparator!==null){
+        newArr.sort(comparator);
+      }
+      else{
+        newArr.sort();
+      }
+
+      for(let i=0; i<indices.length; i++){
+        this.arr[indices[i]]=newArr[i];
+      }
+    
   }
 
   setComparator(compareFunction) {
     // your implementation
+    comparator=compareFunction;
   }
 }
 
